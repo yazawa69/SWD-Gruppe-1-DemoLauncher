47368d8877a634cbb22a8efaeb6ac8e6a0d9d2c8
tiram-isu <93144973+tiram-isu@users.noreply.github.com>
2023-06-14 15:59:42 +0200

    link to landing page

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -8,7 +8,7 @@
         <header class="spec_header">
             <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
                 <div class="container-fluid" id="navbar_container">
-                  <a class="navbar-brand" href="./Index.html">
+                  <a class="navbar-brand" href="{{ route('landing') }}">
                     <img class="logo_image" src="{{ asset('images/Logo.png') }}"></img>
                   </a>
                   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,7 +17,7 @@
                   <div class="collapse navbar-collapse" id="navbarNav">
                     <ul id="nav_list_container" class="navbar-nav">
                       <li class="nav-item">
-                        <a class="nav-link" href="{{ url('/') }}">Startseite</a>
+                        <a class="nav-link" href="{{ route('landing') }}">Startseite</a>
                       </li>
                       <li class="nav-item">
                         <a class="nav-link" href="{{ route('scenarios.index') }}">Szenarien verwalten</a>
