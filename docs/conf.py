402ae6a1544a379751fff6490cb81c376f33ecf0
tiram-isu <93144973+tiram-isu@users.noreply.github.com>
2023-06-25 06:00:42 +0200

    added custom css for docs

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,15 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
+# These folders are copied to the documentation's HTML output
+html_static_path = ['_static']
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+]
+
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
