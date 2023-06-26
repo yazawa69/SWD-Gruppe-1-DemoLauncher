cc148f704e878e603c8579e8fed6ec8a336dd8f4
tiram-isu <93144973+tiram-isu@users.noreply.github.com>
2023-06-26 17:21:19 +0200

    edit csrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'https://swdpr.ddnss.de*'
+        'https://swdpr.ddnss.de*',
+        'http://127.0.0.1:8000*'
     ];
 }
